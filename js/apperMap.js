01fef19b0173c354b469b8a7275b3065fd762a62
Shidatawaiwai <naoshi6a708@gmail.com>
2025-01-19 20:25:09 +0900

    Update apperMap.js

--- js/apperMap.js
+++ js/apperMap.js
@@ -1,21 +1,22 @@
-let apperMap = function(lat,lon,id){
+let apperMap = function(lat,lon,map_id,map_parent_class){
     
     //latitude:緯度;longitude:経度
     
     //地図を表示するスペースを確保
-    let win_width = $(window).width();  //画面幅
-    let selector = $(`#${id}`);
+    let win = $(window);  //画面幅
+    let parent = $(`.${map_parent_class}`);
+    let selector = $(`#${map_id}`);
     let map_len = "500px";  //初期値
-    if(win_width>600){
+    if(win.width()>600){
         map_len = "500px";
     }else{
-        map_len = `${win_width*0.98-30}px`;    //画面幅の85%
+        map_len = `${parent.width()}px`;    //map_idの親要素のwidth
     }
     selector.width(map_len);
     selector.height(map_len);
     
 
-    let map = L.map(id).setView([lat,lon],16);
+    let map = L.map(map_id).setView([lat,lon],16);
     const accessUrl = "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png";
     const copyright = "© <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors";
 
